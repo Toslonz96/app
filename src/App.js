de3cf4457b39cc1f5ec11833ea9a34cc81100620
Toslonz <chutphat23@gmail.com>
2026-01-08 10:25:02 +0700

    Problem with event handler

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import React from 'react';
 import { Header } from './func-components';
 import { Content } from './func-components';
 import { Footer } from './func-components';
-import Calendar from './class-components';
+import Button from './class-components';
 function App() {
   return (
-    <>
-      <Header />
-      <p><center><Calendar /> </center></p>
-      <Content />
-      <Footer />
-    </>
+      <Button />
   )
 }
 export default App;
--- src/class-components.js
+++ src/class-components.js
@@ -1,17 +1,23 @@
 import React,{Component} from "react";
 
-export default class Calendar extends Component {
-    getDate (){
-        const dayNames=['อาทิตย์','จันทร์','อังคาร','พุธ','พฤหัสบดี','ศุกร์','เสาร์'];
-        const monthNames=['มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม'];
-        const date=new Date();
-        const weekDay=dayNames[date.getDay()];
-        const day = date.getDate();
-        const month=monthNames[date.getMonth()];
-        const year=date.getFullYear()+543;
-        return `วัน${weekDay} ที่ ${day} ${month} ${year}`;
+export default class Button extends React.Component {
+    /*constructor(){
+        super()
+        this.onClickButtonOk = this.onClickButtonOk.bind(this);
+    }*/
+    showAlert(msg){
+        alert(msg);
+    }
+
+    onClickButtonOk = () =>{// arrow function to avoid binding.
+        this.showAlert("Hello");
     }
     render(){
-        return (<div>{this.getDate()}</div>)
+        return(
+            <div>
+                <button onClick={this.onClickButtonOk.bind(this)}>OK</button>
+            </div>
+        )
     }
+    
 }
