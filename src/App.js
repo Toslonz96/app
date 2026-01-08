b5a704dd52fcfde6b788300a1dc20e78531c5b50
Toslonz <chutphat23@gmail.com>
2026-01-08 11:13:36 +0700

    Parameter in Event Handler

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Header } from './func-components';
 import { Content } from './func-components';
 import { Footer } from './func-components';
-import Button from './class-components';
+import Button from './class-components'; //call extend Button or not extend { Button }
+import { Calculator2 } from './calculator';
 function App() {
   return (
-      <Button />
+      <Calculator2 />
   )
 }
 export default App;
