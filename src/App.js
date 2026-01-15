73374d7be8f56fcf925d63f9db4dfed203650ed4
Toslonz <chutphat23@gmail.com>
2026-01-15 11:18:05 +0700

    Add setstate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import React from 'react';
-import { Header } from './func-components';
-import { Content } from './func-components';
-import { Footer } from './func-components';
-import Button from './class-components'; //call extend Button or not extend { Button }
-import { Calculator2 } from './calculator';
-import { EventData2 } from './event-data';
-import RefsArray from './ref-array';
+import MessageBox from "./state-func";
 function App() {
   return (
-      <RefsArray />
+      <MessageBox />
   )
 }
 export default App;
